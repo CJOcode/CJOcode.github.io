da36da5365e99a650610db52523aec75b2b1789e
CJOcode <cjoconnor24@gmail.com>
2022-06-22 07:58:11 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('navclicks')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav-buttons");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
